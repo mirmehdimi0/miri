91ce4f866430a88fa0af46c661bcb2275c0ec70d
shuku23 <shukufakhudaverdiyeva@gmail.com>
2023-09-01 09:04:10 -0700

    yeni ders

---
+++ miri/miri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Cem(int x, int y)
+            {
+                int cem = x + y;
+                Console.WriteLine("İki ededin cemi-" + cem);
+            }
+            Cem(5, 4);
+        }
+    }
+}
